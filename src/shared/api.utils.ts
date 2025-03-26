a89d51511b258880af11bf66775ab088fc0ec4a7
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-03-26 02:16:42 +0200

    adds shared api utils code

---
+++ src/shared/api.utils.ts
@@ -0,0 +1,25 @@
+/** Shared API Utils */
+
+import { Merchant } from "../pay-by-link/config";
+
+/** Returns an appropriate api host depending on whether the merchant is in test mode or live mode  */
+export function getApiHost(merchant: Merchant) {
+    const host = merchant.isTestMercant() ? 'eu-test.oppwa.com' : 'eu-prod.oppwa.com';
+    return host;
+}
+
+/** Completes the url for the given an endpoint. Uses the merchant to determine an appropriate host, whether to use test or live host */
+export function getUri(merchant: Merchant, endPoint: string) {
+    const host = getApiHost(merchant);
+    return `https://${host}${endPoint}`;
+}
+
+export function merchantAuthorization(merchant: Merchant) {
+    return {
+        "Authorization": `Bearer ${merchant.getBearerToken()}`
+    }
+}
+
+export function getUriWithEntityParam(merchant: Merchant, endPoint: string) {
+    return `${getUri(merchant, endPoint)}?entityId=${merchant.getEntityId()}`;
+}
